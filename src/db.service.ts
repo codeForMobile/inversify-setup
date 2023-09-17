c0c55276cf8da0173a1d56377371c285d7448798
codeForMobile <code.mobile@protonmail.com>
2023-09-16 19:31:22 -0600

    subscribers route working

--- src/db.service.ts
+++ src/db.service.ts
@@ -1,12 +1,16 @@
 import { injectable } from "inversify"
 import mongoose from "mongoose"
+import { subscriberModel } from "./subscribers.model"
 
 @injectable()
 export class DBService {
+    private _db: typeof mongoose
     async connect(){
-        await mongoose.connect(process.env.DB_URI as string).catch(
-            console.error
-          )
+        this._db = await mongoose.connect(process.env.DB_URI as string)
           console.log('connected to db')
+    }
+
+    get subscriber() {
+        return this._db.model('Subscriber', subscriberModel)
     }
 }
--- src/subscribers.controller.ts
+++ src/subscribers.controller.ts
@@ -1,4 +1,12 @@
-import { controller } from 'inversify-express-utils'
+import { controller, httpGet } from 'inversify-express-utils'
+import { DBService } from './db.service';
 
 @controller('/subscribers')
-export class SubscribersController{}
+export class SubscribersController{
+    constructor (private readonly _dbContext: DBService){}
+
+    @httpGet('/')
+    async index(_req, res){
+        res.json(await this._dbContext.subscriber.find({}))
+    }
+}
